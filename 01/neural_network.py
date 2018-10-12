267ba36a5aedf9c1ce6ca3cd8465f9c2cfa61ee8
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-10-12 14:46:11 +0200

    ReLU and sigmoid init;

--- 01/neural_network.py
+++ 01/neural_network.py
@@ -1,9 +1,11 @@
 import numpy as np
+import math
+
 
 class NeuralNetwork:
 
     def __init__(self, number_of_layers, layers_size_vector, activation_function):
-        if number_of_layers != len(layers_size_vector):
+        if number_of_layers != len(layers_size_vector)-1:
             raise Exception('Inconsistent input parameters!')
         self.activation_function = activation_function
         self.number_of_layers = number_of_layers
@@ -15,7 +17,7 @@ class NeuralNetwork:
         self.bias = dict()
         for i in range(self.number_of_layers):
             self.weights[i] = np.random.rand(layers_size_vector[i+1],layers_size_vector[i])*2-1
-            self.bias[i] = np.random.rand(layers_size_vector[i],1)*2-1
+            self.bias[i] = np.random.rand(layers_size_vector[i+1],1)*2-1
 
     def output(self,input_vector):
         input_vector = [1] + input_vector
@@ -25,3 +27,13 @@ class NeuralNetwork:
         for i in range(self.number_of_layers):
             A = self.activation_function(np.dot(self.weights[i], A) + self.bias[i])
         return A
+
+def ReLU(x):
+    return x * (x > 0)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+if __name__ == '__main__':
+    NN = NeuralNetwork(3,[10,20,20,2],sigmoid)
+    print(NN.output(np.random.rand(10,1)*10))
