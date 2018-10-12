11875b57b3e854277db59b502b118bf3230c7cd9
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-10-12 14:35:30 +0200

    output method init

--- 01/neural_network.py
+++ 01/neural_network.py
@@ -8,6 +8,7 @@ class NeuralNetwork:
         self.activation_function = activation_function
         self.number_of_layers = number_of_layers
         self.initialise_parameters(layers_size_vector)
+        self.layers_size_vector = layers_size_vector
 
     def initialise_parameters(self, layers_size_vector):
         self.weights = dict()
@@ -15,3 +16,12 @@ class NeuralNetwork:
         for i in range(self.number_of_layers):
             self.weights[i] = np.random.rand(layers_size_vector[i+1],layers_size_vector[i])*2-1
             self.bias[i] = np.random.rand(layers_size_vector[i],1)*2-1
+
+    def output(self,input_vector):
+        input_vector = [1] + input_vector
+        if len(input_vector) != self.layers_size_vector[0]:
+            raise Exception('Dimensions mismatch!')
+        A = input_vector
+        for i in range(self.number_of_layers):
+            A = self.activation_function(np.dot(self.weights[i], A) + self.bias[i])
+        return A
