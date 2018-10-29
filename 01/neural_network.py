b352922d79ba6344ba46660cb83ce80c9a0ed20e
Marcin Mazurkiewicz <jaktam.3@gmail.com>
2018-10-29 18:45:32 +0100

    make predict method, but still sth is not working

--- 01/neural_network.py
+++ 01/neural_network.py
@@ -10,7 +10,7 @@ class NeuralNetwork:
     def __init__(self, number_of_layers, layers_size_vector, activation_function, cost_function = 'cross-entropy'):
         if number_of_layers != len(layers_size_vector)-1:
             raise ValueError('Inconsistent input parameters!')
-        self.activation_function = activation_function
+        #self.activation_function = activation_function
         self.number_of_layers = number_of_layers
         self.initialise_parameters(layers_size_vector)
         self.layers_size_vector = layers_size_vector
@@ -44,10 +44,13 @@ class NeuralNetwork:
     def whole_output(self, A):
         # number_of_training_examples = input_matrix.shape[1]
         # A = np.vstack([[1] * number_of_training_examples, input_matrix])
-        for i in range(self.number_of_layers):
+        for i in range(self.number_of_layers-1):
             Z = self.linear_forward(A, i)
             self.cache.append((A, Z))
-            A = self.activation_function(Z)
+            A = ReLU(Z)
+        Z = self.linear_forward(A, self.number_of_layers-1)
+        self.cache.append((A, Z))
+        A = sigmoid(Z)
         return A
 
     def linear_forward(self, previous_A, layer_no):
@@ -59,31 +62,31 @@ class NeuralNetwork:
     #     # print(output)
     #     return np.argmax(output, axis=0)
 
-    def cost_function_evaluation(self, X, Y, _lambda = 0):
+    def cost_function_evaluation(self, X, Y, cost_function):
         output = self.whole_output(X)
-        if self.cost_function == 'cross-entropy':
-            return np.sum(-np.multiply(Y, np.log(output)) - np.multiply((1 - Y), np.log(1 - output))) / len(Y)
-        elif self.cost_function == 'euclidean_distance':
-            return 1/2 * np.sum(np.power(Y-output,2)) / len(Y)
+        #if cost_function == 'cross-entropy':
+        #    return np.sum(-np.multiply(Y, np.log(output)) - np.multiply((1 - Y), np.log(1 - output))) / len(Y)
+        #elif cost_function == 'euclidean_distance':
+        return 1/2 * np.sum(np.power(Y-output,2)) / len(Y)
 
-    def output_layer_cost_derivative(self, output_matrix, Y):
-        if self.cost_function == 'cross-entropy':
-            return - (np.divide(Y, output_matrix.T) - np.divide(1 - Y, 1 - output_matrix.T))
-        elif self.cost_function == 'euclidean_distance':
-            return -(Y - output_matrix)
+    def output_layer_cost_derivative(self, output_matrix, Y, cost_function):
+ #       if cost_function == 'cross-entropy':
+ #           return - (np.divide(Y, output_matrix.T) - np.divide(1 - Y, 1 - output_matrix.T))
+  #      elif cost_function == 'euclidean_distance':
+        return -(Y - output_matrix.T)
 
     def back_propagation(self, X, Y, regularisation_lambda = 0): #To use regularization we should change backpropagation as well. Dont know if its taken into consideration
         self.cost_derivatives = dict()
         self.weight_derivatives = dict()
         self.bias_derivatives = dict()
-        self.cost_derivatives[self.number_of_layers - 1] = self.output_layer_cost_derivative(self.whole_output(X), Y)
+        self.cost_derivatives[self.number_of_layers - 1] = self.output_layer_cost_derivative(self.whole_output(X), Y, 'cross-entropy')
         dZ = self.cost_derivatives[self.number_of_layers - 1].T
         for i in reversed(range(self.number_of_layers)):
             self.weight_derivatives[i] = (np.dot(dZ, self.cache[i][0].T)) / X.shape[1]
             # self.bias_derivatives[i] = np.squeeze(np.sum(dZ, axis=1, keepdims=True)) / X.shape[1]
             self.bias_derivatives[i] = np.sum(dZ, axis=1, keepdims=True) / X.shape[1]
             self.cost_derivatives[i - 1] = np.dot(self.weights[i].T, dZ)
-            dZ = self.cost_derivatives[i-1] * self.activation_function(self.cache[i-1][1], grad=True)
+            dZ = self.cost_derivatives[i-1] * ReLU(self.cache[i-1][1], grad=True)
 
     def update_weights(self, learning_rate):
         for i in range(self.number_of_layers):
@@ -91,7 +94,7 @@ class NeuralNetwork:
             # print(self.bias_derivatives[i],self.bias[i])
             self.bias[i] -= learning_rate * self.bias_derivatives[i]
 
-    def fit(self, X, Y, learning_rate, regularisation_lambda, epsilon,max_iteration_number = 10000, min_max_normalization = False):
+    def fit(self, X, Y, learning_rate, regularisation_lambda, epsilon,max_iteration_number = 2000, min_max_normalization = False):
         regularisation_lambda = 0
         costs = []
         if not self.fitted:
@@ -102,16 +105,28 @@ class NeuralNetwork:
             #while ((self.cost_function_evaluation(X, Y) / previous_cost_function <= epsilon) and (counter<max_iteration_number)) or (counter<5):
             for i in range(max_iteration_number):
                 # print(counter, previous_cost_function)
-                previous_cost_function = self.cost_function_evaluation(X,Y)
+                previous_cost_function = self.cost_function_evaluation(X,Y, 'cross-entropy')
                 self.back_propagation(X, Y, regularisation_lambda)
                 self.update_weights(learning_rate)
                 #counter +=1
                 # print(counter, self.cost_function(X,Y)/previous_cost_function)
-                costs.append(self.cost_function_evaluation(X,Y))
+                costs.append(self.cost_function_evaluation(X,Y, 'cross-entropy'))
+                if i % 1000 == 0:
+                    print("Cost after iteration {}: {}".format(i, costs[i]))
             self.fitted = True
             return costs
         else:
             raise Exception("Neural network already fitted!")
+
+    def predict(self, A):
+        result = []
+        A = A.T
+        for i in range(len(A)):
+            if A[i] >= .5:
+                result.append(1)
+            else:
+                result.append(0)
+        return result
 
     # def set_weights(self, list_of_parameters):
     #     for layer_no, parameters in enumerate(list_of_parameters):
@@ -140,12 +155,18 @@ def getSamples_array(N):
     return X,Y
 
 if __name__ == '__main__':
-    X, Y = getSamples_array(10)
+    X, Y = getSamples_array(100)
     NN = NeuralNetwork(3, [X.shape[0], 20, 3, 1], sigmoid, 'cross-entropy')
-    NN.initialise_parameters(NN.layers_size_vector)
-    A = NN.whole_output(X)
-    NN.back_propagation(X, Y)
-    print(NN.weight_derivatives)
+    #NN.initialise_parameters(NN.layers_size_vector)
+    # A = NN.whole_output(X)
+    # NN.back_propagation(X, Y)
+    # NN.update_weights(.01)
+    NN.fit(X, Y, .01, 1,1)
+    result = NN.whole_output(X)
+    output = NN.predict(result)
+    print(output)
+
+
 
     # print(NN.single_output((np.ones((9, 1)) * 10)))
     # print(np.multiply(NN.whole_output(np.ones((9, 3)) * 10),np.ones((2,3))))
--- 01/plot.py
+++ 01/plot.py
@@ -41,21 +41,28 @@ def getSamples(N):
     return samples
 
 
-def getSamples_array(N, balanced = False):
+# def getSamples_array(N, balanced = False):
+#     X = np.random.normal(size=(2, N))
+#     Y = np.zeros((N), dtype='int')
+#     for i in range(N):
+#         if (X[0, i] > 0) and (X[1, i] < 0):
+#         # if X[1, i] > 0:
+#             Y[i] = 1
+#         #else:
+#         #    Y[0, i] = 1
+#     if balanced:
+#         X_extra = np.repeat(X[:,Y[1,:]==1],3,axis=1)
+#         Y_extra = np.zeros(X_extra.shape, dtype='int')
+#         Y_extra[1,:] = 1
+#         X = np.hstack((X,X_extra))
+#         Y = np.hstack((Y,Y_extra))
+#     return X,Y
+def getSamples_array(N):
     X = np.random.normal(size=(2, N))
-    Y = np.zeros((N), dtype='int')
+    Y = np.zeros((N,1))#, dtype='int')
     for i in range(N):
         if (X[0, i] > 0) and (X[1, i] < 0):
-        # if X[1, i] > 0:
             Y[i] = 1
-        #else:
-        #    Y[0, i] = 1
-    if balanced:
-        X_extra = np.repeat(X[:,Y[1,:]==1],3,axis=1)
-        Y_extra = np.zeros(X_extra.shape, dtype='int')
-        Y_extra[1,:] = 1
-        X = np.hstack((X,X_extra))
-        Y = np.hstack((Y,Y_extra))
     return X,Y
 
 
@@ -96,13 +103,16 @@ numberOfSamples = 1000
 viewX = [-4, 4, 101]
 viewY = [-4, 4, 101]
 
-X,Y = getSamples_array(numberOfSamples)
-NN = NeuralNetwork(3, [2, 10, 10, 2], sigmoid,'cross-entropy')
+#X,Y = getSamples_array(numberOfSamples)
+X, Y = getSamples_array(100)
+#NN = NeuralNetwork(3, [2, 10, 10, 2], sigmoid,'cross-entropy')
+NN = NeuralNetwork(3, [X.shape[0], 20, 3, 1], sigmoid, 'cross-entropy')
 # NN = NeuralNetwork(3, [2, 20,20, 2], sigmoid,'euclidean_distance')
 # NN = NeuralNetwork(2, [2, 200, 2], sigmoid,'euclidean_distance')
-costs = NN.fit(X, Y, 0.03, 3, 0.9999, 10000)
-plt.plot(costs,'o-')
-plt.show()
+#costs = NN.fit(X, Y, 0.03, 3, 0.9999, 10000)
+NN.fit(X, Y, .01, 1,1)
+#plt.plot(costs,'o-')
+#plt.show()
 # NN = NeuralNetwork(2, [2, 2, 2], sigmoid)
 # NN.set_weights([(np.array([[1, 0.01],[0.01, 1]]), np.array([[0],[0]])),(np.array([[1, -1],[-1, 1]]),np.array([[0.3],[-0.3]]))])
 plotDecisionDomain_our(getGrid(viewX), getGrid(viewY), getDecisionOfFakeNeuralNet_our, NN.whole_output)
