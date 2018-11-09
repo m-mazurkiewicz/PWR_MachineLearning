b306d3b3746ac4062163b9f6f8fdb3f869382a11
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-11-09 11:57:11 +0100

    mnist_loader - list comprehension for one-hot encoding added

--- 01/mnist_loader.py
+++ 01/mnist_loader.py
@@ -63,9 +63,9 @@ def load_data_wrapper():
     training_results = [vectorized_result(y) for y in tr_d[1]]
     training_data = (training_inputs, training_results) #zip(training_inputs, training_results)
     validation_inputs = [np.reshape(x, (784, 1)) for x in va_d[0]]
-    validation_data = (validation_inputs, va_d[1]) #zip(validation_inputs, va_d[1])
+    validation_data = (validation_inputs, [vectorized_result(y) for y in va_d[1]]) #zip(validation_inputs, va_d[1])
     test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
-    test_data = (test_inputs, te_d[1]) #zip(test_inputs, te_d[1])
+    test_data = (test_inputs, [vectorized_result(y) for y in te_d[1]]) #zip(test_inputs, te_d[1])
     return (training_data, validation_data, test_data)
 
 def vectorized_result(j):
