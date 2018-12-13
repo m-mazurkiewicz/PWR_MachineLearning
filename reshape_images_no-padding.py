60ab62ca36a3791a32f54ffa7b72beda35eade7a
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-12-13 14:37:13 +0100

    reshape_images_no-padding

--- reshape_images_no-padding.py
+++ reshape_images_no-padding.py
@@ -24,22 +24,35 @@ def resize_image(image_path, image_name, resample_method):
 
 
 def crop_image(image_path, image_name, resample_method):
+    '''
+    Crop image in the middle of the image in square shape of the biggest possible size
+    :param image_path:
+    :param image_name:
+    :param resample_method:
+    '''
     with open(image_path + image_name, 'r+b') as f:
         with Image.open(f) as image:
-            width, height = image.size
-            if width <= height:
-                img = resizeimage.resize_width(image, 227, resample=resample_method)
+            width, height = image.size  # Get dimensions
+            if width<=height:
+                new_width = width
+                new_height = width
             else:
-                img = resizeimage.resize_height(image, 227, resample=resample_method)
-            img = resizeimage.resize_crop(img, size=[227, 227])
-            img.save(image_path.replace('raw/', 'interim/no-padding/crop/') + image_name, img.format)
+                new_width = height
+                new_height = height
+            left = (width - new_width) / 2
+            top = (height - new_height) / 2
+            right = (width + new_width) / 2
+            bottom = (height + new_height) / 2
+            img = image.crop((left, top, right, bottom))
+            img = img.resize(size=[227, 227], resample=resample_method)
+            img.save(image_path.replace('raw/', 'interim/no-padding/crop/') + image_name, image.format)
 
 
 def proceed_class(class_name, reshape_method, resample_method):
     if reshape_method == 'crop':
         os.makedirs((base_dir + class_name + '/').replace('raw/', 'interim/no-padding/crop/'), exist_ok=True)
         for key,image in enumerate(os.listdir(base_dir + class_name)):
-            resize_image(base_dir + class_name + '/', image, resample_methods_dict[resample_method])
+            crop_image(base_dir + class_name + '/', image, resample_methods_dict[resample_method])
             if key % 100 == 0:
                 print(key)
     elif reshape_method == 'resize':
